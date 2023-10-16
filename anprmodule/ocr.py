de951ee29d361b25af235fd37c4224e4c1517a69
Jonas <jonas@Jonass-MacBook-Pro.local>
2023-10-16 11:46:01 +0200

    update

--- anprmodule/ocr.py
+++ anprmodule/ocr.py
@@ -1,13 +1,21 @@
 import cv2
+import numpy as np
 import keras_ocr
+import pytesseract
+import PIL
 
 def ocr_image(img, coordinates):
     pipeline = keras_ocr.pipeline.Pipeline()
     x, y, w, h = int(coordinates[0]), int(coordinates[1]), int(coordinates[2]), int(coordinates[3])
     img = img[y:h, x:w]
-
-    img = cv2.resize(img, None, fx = 3, fy = 3, interpolation = cv2.INTER_CUBIC)
-
+    img = cv2.resize(img, None, fx = 1, fy = 1, interpolation = cv2.INTER_CUBIC)
+    #print(img.shape)
+    #cv2.imshow('License Plate', img)
+    #cv2.waitKey(0)
+    kernel3 = np.array([[0, -1, 0],
+                        [-1, 5, -1],
+                        [0, -1, 0]])
+    img = cv2.filter2D(src=img, ddepth=-1, kernel=kernel3)
     text = ""
     pred = pipeline.recognize([img])
     # pred contains predicted text in the input image. Some number plates have text on the black border.
--- anprmodule/predict.py
+++ anprmodule/predict.py
@@ -1,5 +1,4 @@
-import os
-
+import cv2
 import hydra
 import logging
 import numpy as np
@@ -8,13 +7,19 @@ from ultralytics.yolo.engine.predictor import BasePredictor
 from ultralytics.yolo.utils import ops
 from ultralytics.yolo.utils.checks import check_imgsz
 
+
+from omegaconf import OmegaConf
+
 from anprmodule.ocr import ocr_image
-from .config_loader import get_config
+#from config_loader import get_config
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.FATAL)
 reg = ''
+_DICT = {'task': 'detect', 'mode': 'train', 'model': None, 'data': None, 'epochs': 100, 'patience': 50, 'batch': 16, 'imgsz': 640, 'save': True, 'cache': False, 'device': None, 'workers': 8, 'project': None, 'name': None, 'exist_ok': False, 'pretrained': False, 'optimizer': 'SGD', 'verbose': False, 'seed': 0, 'deterministic': True, 'single_cls': False, 'image_weights': False, 'rect': False, 'cos_lr': False, 'close_mosaic': 10, 'resume': False, 'overlap_mask': True, 'mask_ratio': 4, 'dropout': 0.0, 'val': True, 'save_json': False, 'save_hybrid': False, 'conf': None, 'iou': 0.7, 'max_det': 300, 'half': False, 'dnn': False, 'plots': True, 'source': None, 'show': False, 'save_txt': False, 'save_conf': False, 'save_crop': False, 'hide_labels': False, 'hide_conf': False, 'vid_stride': 1, 'line_thickness': 3, 'visualize': False, 'augment': False, 'agnostic_nms': False, 'retina_masks': False, 'format': 'torchscript', 'keras': False, 'optimize': False, 'int8': False, 'dynamic': False, 'simplify': False, 'opset': 17, 'workspace': 4, 'nms': False, 'lr0': 0.01, 'lrf': 0.01, 'momentum': 0.937, 'weight_decay': 0.0005, 'warmup_epochs': 3.0, 'warmup_momentum': 0.8, 'warmup_bias_lr': 0.1, 'box': 7.5, 'cls': 0.5, 'dfl': 1.5, 'fl_gamma': 0.0, 'label_smoothing': 0.0, 'nbs': 64, 'hsv_h': 0.015, 'hsv_s': 0.7, 'hsv_v': 0.4, 'degrees': 0.0, 'translate': 0.1, 'scale': 0.5, 'shear': 0.0, 'perspective': 0.0, 'flipud': 0.0, 'fliplr': 0.5, 'mosaic': 1.0, 'mixup': 0.0, 'copy_paste': 0.0, 'hydra': {'output_subdir': None, 'run': {'dir': '${hydra:runtime.cwd}/${now:%Y-%m-%d_%H-%M-%S}'}}, 'v5loader': False}
 
+def get_config():
+    return OmegaConf.create(_DICT)
 class DetectionPredictor(BasePredictor):
 
     def preprocess(self, img):
@@ -78,6 +83,6 @@ def run(src, model):
 
 if __name__ == "__main__":
     model = '../lib/best.pt'
-    source = '../lib/EWP05W.jpg'
+    source = ('truck2.jpg')
     r = run(src=source, model=model)
     print(r)
